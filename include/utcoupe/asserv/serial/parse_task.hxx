7c31c15dc100b0b64ce3bd08da7a4b5b9d4a7084
Mindstan <mindstan@hotmail.fr>
2020-10-11 21:39:32 +0200

    Added parsers (not tested yet)

--- include/utcoupe/asserv/serial/parse_task.hxx
+++ include/utcoupe/asserv/serial/parse_task.hxx
@@ -3,7 +3,58 @@
 
 #include "utcoupe/asserv/serial/protocol.hpp"
 
+#include <charconv>
+#include <functional>
+#include <optional>
+#include <utility>
+
 namespace utcoupe::asserv::serial {
+    
+    /**
+     * Parse a parameter given in a string, and returns the parsed value.
+     * 
+     * Compatible types are all integer types, float, double, long double.
+     * If it fails to parse, it will return std::nullopt.
+     * 
+     * @param first Address of first character of the string
+     * @param last Address + 1 of the last character of the string
+     * @return The parsed value
+     */
+    template<typename ArgT>
+    std::optional<ArgT> parseParameters(const char* first, const char* last) {
+        ArgT val;
+        auto result = std::from_chars(first, last, val);
+        if (result.ec != std::errc{}) {
+            return std::nullopt;
+        }
+        return val;
+    }
+    
+    /**
+     * Parse parameters given in a string, and returns filled arguments in a tuple.
+     * 
+     * Compatible types are all integer types, float, double, long double.
+     * Parameters must be separed by a character like ' ', ';', ..., that can't be interpreted as part of a number. See https://en.cppreference.com/w/cpp/utility/from_chars for more details
+     * If the number of parameters is smaller than the number of arguments to fill, it will return std::nullopt.
+     * 
+     * @param first Address of first character of the string
+     * @param last Address + 1 of the last character of the string
+     * @return A tuple containing values for all given argument types, in the same order, or std::nullopt if it has failed
+     */
+    template<typename CurArgT, typename... OtherArgsT>
+    std::optional<std::tuple<CurArgT, OtherArgsT...>> parseParameters(const char* first, const char* last) {
+        CurArgT val;
+        auto [nextFirst, ec] = std::from_chars(first, last, val);
+        if (ec != std::errc{} || nextFirst + 1 >= last) {
+            return std::nullopt;
+        }
+        
+        auto otherValues = parseParameters<OtherArgsT...>(nextFirst + 1, last);
+        if (!otherValues) {
+            return std::nullopt;
+        }
+        return std::tuple_cat(val, otherValues);
+    }
     
     template<TasksDispatcherLike TTasksDispatcher>
     bool parseTask(std::string_view msg, TTasksDispatcher& tasksDispatcher) {
