c420e7b86015c417013b7b39f5fb919bb480707b
Mindstan <mindstan@hotmail.fr>
2021-01-14 21:04:36 +0100

    Removes prev protocol parser

--- include/utcoupe/asserv/serial/protocol.hpp
+++ include/utcoupe/asserv/serial/protocol.hpp
@@ -4,24 +4,6 @@
 #include "utcoupe/asserv/serial/order.hpp"
 
 namespace utcoupe::asserv::serial {
-    enum class TaskTypes {
-        CLEAN_GOALS = 'g',
-        GET_CODER = 'j',
-        GET_LAST_ID = 't',
-        GET_POS = 'n',
-        GET_POS_ID = 'o',
-        GET_SPD = 'y',
-        GET_TARGET_SPD = 'v',
-        GOTO_WITH_ANGLE = 'c',
-        HALT = 'H',
-        KILL_GOAL = 'f',
-        PAUSE = 'q',
-        RESET_ID = 's',
-        RESUME = 'r',
-        // SET_EMERGENCY_STOP = 'A',
-        START = 'S',
-        WHO_AMI = 'w'
-    };
     
     enum class OrderTypes : char {
         ACC_MAX = 'l',
