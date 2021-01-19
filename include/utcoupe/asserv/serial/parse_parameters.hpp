d9773fa101966b7e570b91085610a64f9106405e
Mindstan <mindstan@hotmail.fr>
2021-01-19 21:15:51 +0100

    Added FP support when std::from_chars<FPType> is not available

--- include/utcoupe/asserv/serial/parse_parameters.hpp
+++ include/utcoupe/asserv/serial/parse_parameters.hpp
@@ -4,10 +4,44 @@
 #include "utcoupe/asserv/serial/traits.hpp"
 
 #include <charconv>
+#include <cstdlib>
+#include <concepts>
 #include <optional>
 #include <tuple>
 
 namespace utcoupe::asserv::serial {
+    namespace impl {
+        /**
+         * Helper when std::from_chars for floating-point types is not available.
+         * 
+         * It internaly uses std::strtof/strtod/strtold, which are not as fast as std::from_chars<double>(...). It also does NOT CHECK boundaries, the string MUST either ends with '\0' or contain a char that isn't a part of a floating point value string representation.
+         * @param first The address of the first char of the string
+         * @param value The variable to set in case of success, or remain untouched
+         * @return The address +1 of the last parsed char, or std::nullopt in case of error
+         */
+        template<std::floating_point ArgT>
+        std::optional<const char *> from_chars_fp(const char* first, ArgT& value) {
+            char* last = nullptr;
+            ArgT result;
+            
+            // std::remove_cv_t is not be needed
+            if constexpr (std::is_same_v<ArgT, float>) {
+                result = std::strtof(first, &last);
+            } else if constexpr (std::is_same_v<ArgT, double>) {
+                result = std::strtod(first, &last);
+            } else /* if constexpr (std::is_same_v<ArgT, long double>) */ {
+                result = std::strtold(first, &last);
+            }
+            
+            // std::strtof/strtod/strtold sets the value of last to first if it failed to parse.
+            if (last == first) {
+                return std::nullopt;
+            }
+            value = result;
+            return last;
+        }
+    } // namespace impl
+    
     /**
      * Helper when std::from_chars for current type is not available.
      * 
@@ -17,6 +51,34 @@ namespace utcoupe::asserv::serial {
     template<typename CurArgT, typename... OtherArgsT>
     std::optional<std::tuple<CurArgT, OtherArgsT...>> parseParameters([[maybe_unused]] const char* first, [[maybe_unused]] const char* last) noexcept {
         return std::nullopt;
+    }
+    
+    /**
+     * Helper when std::from_chars for floating-point types is not available.
+     */
+    template<std::floating_point CurArgT, typename... OtherArgsT>
+    requires (not CanIntantiateFromChars<CurArgT>)
+    std::optional<std::tuple<float, OtherArgsT...>> parseParameters(const char* first, const char* last) noexcept {
+        if (first + 1 >= last) {
+            return std::nullopt;
+        }
+        CurArgT val;
+        auto nextFirstOpt = impl::from_chars_fp(first, val);
+        if (!nextFirstOpt) {
+            return std::nullopt;
+        }
+        
+        // Same as general parseParameters
+        if constexpr (sizeof ...(OtherArgsT) == 0) {
+            // TODO maybe warn unused parameters? (if nextFirst + 1 < last)
+            return std::tuple{ val };
+        } else {
+            auto otherValues = parseParameters<OtherArgsT...>(*nextFirstOpt + 1, last);
+            if (!otherValues) {
+                return std::nullopt;
+            }
+            return std::tuple_cat(std::tuple{ val }, otherValues.value());
+        }
     }
     
     
--- test/serial/order_parser_tests.cpp
+++ test/serial/order_parser_tests.cpp
@@ -14,7 +14,8 @@ struct Tester {
     using OrderReturnT = int;
     
     constexpr int run1(int x) { return x; }
-    constexpr int run2(float x) { return static_cast<int>(std::ceil(x)); }
+    constexpr int run2(float x) { return static_cast<int>(std::floor(x)); }
+    constexpr int run3(int x, int y) { return x + y; }
     constexpr int compute1() { return 42; }
     constexpr int compute2() { return 43; }
 };
@@ -48,6 +49,31 @@ suite orderParser = [] {
                 
                 then ("It shouldn't be executable") = [&] {
                     expect (!parser.parseAndRunOrder(allOrders, orderStr));
+                };
+            };
+        };
+        
+        scenario("Argument parsing") = [&] {
+            auto allOrders = std::tuple{
+                serial::createOrder('a', &Tester::run1),
+                serial::createOrder('b', &Tester::run2),
+                serial::createOrder('c', &Tester::run3),
+            };
+            
+            given ("An order with one argument correctly formed") = [&] {
+                std::string_view orderStr1 = "a;5;";
+                std::string_view orderStr2 = "b;4.3;";
+                std::string_view orderStr3 = "c;4;3;";
+                
+                then ("Arguments should be correctly parsed") = [&] {
+                    auto result1 = parser.parseAndRunOrder(allOrders, orderStr1);
+                    expect(*result1 == 5_i);
+                    
+                    auto result2 = parser.parseAndRunOrder(allOrders, orderStr2);
+                    expect(*result2 == 4_i);
+                    
+                    auto result3 = parser.parseAndRunOrder(allOrders, orderStr3);
+                    expect(*result3 == 7_i);
                 };
             };
         };
